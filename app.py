5b4af932cbaf1de522fd64726614a576c70850b6
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-09-19 20:48:11 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ import numpy as np
 from botocore.client import Config
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-# from gtts import gTTS
-# from twilio.rest import Client
+
 
 # from config import ACCESS_KEY_ID, ACCESS_SECRET_KEY, BUCKET_NAME, config
 from config import config
 from crop_recommendation.corp_prediction import recommend_crop
 from crop_recommendation.weather import weather_fetch
 from disease_classifier.classify_disease import predict_image
-# from farmers_log.search_user_request import search_log
 from fertilizier_predict.crop_type_encoder import encode_crop_type
 from fertilizier_predict.decode_fertilizer import decode_fertilizer
 from fertilizier_predict.fertilizer_report import generate_fertilizer_report
@@ -33,7 +31,7 @@ CORS(app)
     # Cultivation
     # Harvest
 
-@app.route("/test", methods = ["GET"])
+@app.route("/", methods = ["GET"])
 def test():
     return response_payload(True, "Hello World Crop Prediction model ka backend start ho chukaa hai")
 
@@ -177,71 +175,7 @@ def predict_fertilizer():
         print(e)
         return response_payload(False, msg="Request body is not valid")
 
-# @app.route('/find_response/<lang>/<phone_number>/<message_body>', methods=['GET','POST'])
-# def find_response(lang,phone_number,message_body):
-#     try:
-#         resp = farmers_log(query = {"log":message_body, "lang":lang})
-            
-#         if resp["success"] == True :
-#             response = resp["data"]["organic_result_1"]["response"]
-#             print('Response length', len(response))
-#             # Convert text to audio
-#             mytext = response
-#             languaje = lang#"en"#es
-#             myobj = gTTS(text=mytext, lang=languaje, slow=False)
-#             file_name ="response23.mp3" 
-#             myobj.save(file_name)   
-            
-#             # Send the audio in to bucket
-#             s3 = boto3.resource(
-#             's3',
-#             aws_access_key_id = ACCESS_KEY_ID,
-#             aws_secret_access_key = ACCESS_SECRET_KEY,
-#             config=Config(signature_version='s3v4')
-#             )
-#             data = open(file_name, 'rb')
-#             s3.Bucket(BUCKET_NAME).put_object(Key=file_name, Body=data, ContentType='audio/mp3')      
-            
-#             s3_url = f"https://{BUCKET_NAME}.s3.{'us-east-1'}.amazonaws.com/{file_name}"
-            
-            
-#             # Send sms
-#             def sms_response(phone_number,response, s3_url):
-#                 # account_sid = "ACa14fbcbce98e84a08d6a60bbdebbf18b"
-#                 # auth_token = "9a9bf0f756d4d38fcbb305e252c27f4a"
 
-#                 account_sid = "AC87dddab50fb1e07d36c39e956715c69a"
-#                 auth_token = "d5bd5d9456abc43638a2a8ce18aac85c"
-                
-#                 client = Client(account_sid, auth_token)
-#                 message = client.messages.create(
-#                     body = response +  s3_url,
-#                     from_ = "+15139607276",#"+19452392171", 
-#                     to = phone_number        
-#                 )
-                
-#             try:
-#                 print('Sending Message')
-#                 sms_response(phone_number, response, s3_url)
-#                 return response_payload(True,'', 'Succesful sent')
-#             except Exception as ex:
-#                 print(ex)
-#                 return jsonify({'message':"Check your internet conection."})
-            
-                
-#             return jsonify({ 'Abra el siguiente enlace para escuchar la respuesta': s3_url,  'message':response,'topic':"Topic found."})
-#         else:    
-#             return jsonify({'message':"Topic not found."})
-#     except Exception as ex:
-#         print(ex)
-#         return jsonify({'message':"Error"})
-def response_payload(success, data, message):
-    response = {
-        "success": success,
-        "data": data,
-        "message": message
-    }
-    return jsonify(response)
 
 @app.route('/disease-predict/<lang>', methods=['GET', 'POST'])
 def disease_prediction(lang):
